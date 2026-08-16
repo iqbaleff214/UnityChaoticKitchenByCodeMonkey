04f918e7b760f2a068d6880970b7a78af97f3347
M. Iqbal Effendi <iqbaleff214@gmail.com>
2026-08-17 05:06:40 +0800

    feat: player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] private float movementSpeed = 7f;
+    [SerializeField] private GameInput gameInput;
+
+    private bool isWalking;
+
+    private void Update()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 movement = new(inputVector.x, 0, inputVector.y);
+
+        float movementDistance = movementSpeed * Time.deltaTime;
+        float playerRadius = .7f;    
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, movement, movementDistance);
+
+        if (!canMove)
+        {
+            // Cannot move towards movement direction
+
+            // Attempt only X movement
+            Vector3 movementX = new(movement.x, 0, 0);
+            movementX.Normalize();
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, movementX, movementDistance);
+
+            if (canMove)
+            {
+                // Can move only on the X
+                movement = movementX;
+            }
+            else
+            {
+                // Cannot move only on the X
+
+                // Attempt only Z movement
+                Vector3 movementZ = new(0, 0, movement.z);
+                movementZ.Normalize();
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, movementZ, movementDistance);
+
+                if (canMove)
+                {
+                    // Can move only on the Z
+                    movement = movementZ;
+                }
+                else
+                {
+                    // Cannot move in any direction
+                }
+            }
+        }
+
+        if (canMove)
+        {
+            transform.position += movementSpeed * Time.deltaTime * movement;
+        }
+
+        isWalking = movement != Vector3.zero;
+
+        float rotationSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, movement, Time.deltaTime * rotationSpeed);
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+}
---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerAnimator : MonoBehaviour
+{
+    private const string IS_WALKING = "IsWalking";
+
+    [SerializeField] private Player player;
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetBool(IS_WALKING, player.IsWalking());
+    }
+
+    private void Update()
+    {
+        animator.SetBool(IS_WALKING, player.IsWalking());
+    }
+}
