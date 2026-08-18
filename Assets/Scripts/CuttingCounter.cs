5f5bdce674b0e8a9a44a86dd5feb6527a4329502
M. Iqbal Effendi <iqbaleff214@gmail.com>
2026-08-19 05:30:30 +0800

    feat: cutting counter visual and animation

--- Assets/Scripts/CuttingCounter.cs
+++ Assets/Scripts/CuttingCounter.cs
@@ -1,9 +1,20 @@
+using System;
 using UnityEngine;
 
 public class CuttingCounter : BaseCounter
 {
 
+    public event EventHandler<OnProgressChangedEventArgs> OnProgressChanged;
+    public class OnProgressChangedEventArgs : EventArgs
+    {
+        public float progressNormalized;
+    }
+    public event EventHandler OnCut;
+
+
     [SerializeField] private CuttingRecipeSO[] cuttingRecipes;
+
+    private int cuttingProgress;
 
     public override void Interact(Player player)
     {
@@ -13,6 +24,11 @@ public class CuttingCounter : BaseCounter
             {
                 // Player is carrying a KitchenObject that can be cut, place it on the counter
                 player.GetKitchenObject().SetKitchenObjectParent(this);
+                cuttingProgress = 0;
+                OnProgressChanged?.Invoke(this, new OnProgressChangedEventArgs
+                {
+                    progressNormalized = (float) cuttingProgress / GetCuttingRecipeSOWithInput(GetKitchenObject().GetKitchenObjectSO()).cuttingProgressMax
+                });
             }
         } 
         else
@@ -35,36 +51,51 @@ public class CuttingCounter : BaseCounter
 
             if (outputKitchenObjectSO != null)
             {
-                // Remove the input KitchenObject from the counter
-                GetKitchenObject().DestroySelf();
+                cuttingProgress++;
+                OnCut?.Invoke(this, EventArgs.Empty);
 
-                // Spawn the output KitchenObject and give it to the player
-                KitchenObject.SpawnKitchenObject(outputKitchenObjectSO, this);
+                CuttingRecipeSO cuttingRecipe = GetCuttingRecipeSOWithInput(inputKitchenObjectSO);
+
+                if (cuttingProgress >= cuttingRecipe.cuttingProgressMax)
+                {
+                    // Cutting is complete, perform the cutting action
+                    // PerformCuttingAction(player, outputKitchenObjectSO);
+                    
+                    // Remove the input KitchenObject from the counter
+                    GetKitchenObject().DestroySelf();
+
+                    // Spawn the output KitchenObject and give it to the player
+                    KitchenObject.SpawnKitchenObject(outputKitchenObjectSO, this);
+                }
+
+                OnProgressChanged?.Invoke(this, new OnProgressChangedEventArgs
+                {
+                    progressNormalized = (float) cuttingProgress / cuttingRecipe.cuttingProgressMax
+                });
             }
         }
     }
 
     private KitchenObjectSO GetOutputForInput(KitchenObjectSO inputKitchenObjectSO)
     {
-        for (int i = 0; i < cuttingRecipes.Length; i++)
-        {
-            if (cuttingRecipes[i].input == inputKitchenObjectSO)
-            {
-                return cuttingRecipes[i].output;
-            }
-        }
-        return null;
+        CuttingRecipeSO cuttingRecipe = GetCuttingRecipeSOWithInput(inputKitchenObjectSO);
+        return cuttingRecipe != null ? cuttingRecipe.output : null;
     }
 
     private bool HasRecipeWithInput(KitchenObjectSO inputKitchenObjectSO)
+    {
+        return GetCuttingRecipeSOWithInput(inputKitchenObjectSO) != null;
+    }
+
+    private CuttingRecipeSO GetCuttingRecipeSOWithInput(KitchenObjectSO inputKitchenObjectSO)
     {
         for (int i = 0; i < cuttingRecipes.Length; i++)
         {
             if (cuttingRecipes[i].input == inputKitchenObjectSO)
             {
-                return true;
+                return cuttingRecipes[i];
             }
         }
-        return false;
-    }
+        return null;
+    }    
 }
---
+++ Assets/Scripts/ProgressBarUI.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBarUI : MonoBehaviour
+{
+    [SerializeField] private Image progressBarImage;
+    [SerializeField] private CuttingCounter cuttingCounter;
+
+    private void Start()
+    {
+        cuttingCounter.OnProgressChanged += CuttingCounter_OnProgressChanged;
+
+        progressBarImage.fillAmount = 0f;
+        
+        Hide();
+    }
+
+    private void CuttingCounter_OnProgressChanged(object sender, CuttingCounter.OnProgressChangedEventArgs e)
+    {
+        progressBarImage.fillAmount = e.progressNormalized;
+
+        if (e.progressNormalized > 0f && e.progressNormalized < 1f)
+        {
+            Show();
+        }
+        else
+        {
+            Hide();
+        }
+    }
+
+    private void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
